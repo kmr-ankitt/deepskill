960db319362024d0c103d8ad6535f652e7fc449b
ankit <sahuankit.murhu@gmail.com>
2026-06-22 23:56:43 +0530

    feat(sql): exercise 5

---
+++ advanced sql/4. sql exercise - stored procedure/Exercise5_ReturnDataFromStoredProcedure.sql
@@ -0,0 +1,16 @@
+USE OnlineRetailStore;
+GO
+
+CREATE PROCEDURE sp_GetEmployeeCountByDepartment
+    @DepartmentID INT
+AS
+BEGIN
+    SELECT COUNT(*) AS TotalEmployees
+    FROM Employees
+    WHERE DepartmentID = @DepartmentID;
+END;
+GO
+
+
+EXEC sp_GetEmployeeCountByDepartment 3;
+GO
